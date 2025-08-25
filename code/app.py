3880dac4c29c9dcb13de198e06621d750896038a
fenixonline <fenixonline@gmail.com>
2025-08-25 18:24:23 -0300

    updating code

--- code/app.py
+++ code/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World from Flask</h2>'
+def hello():
+    return "Hello from EKS!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
