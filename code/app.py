0adeb4f7a18e79c7ab93fc83b51ee59ef895e691
fenixonline <fenixonline@gmail.com>
2025-08-25 16:47:53 -0300

    code modification

--- code/app.py
+++ code/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_world 1():
     return '<h1>Hello World from Flask</h2>'
 
 if __name__ == "__main__":
